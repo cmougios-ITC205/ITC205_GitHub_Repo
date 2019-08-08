3036d8aa0322102cdc1516dae7f06b5572f8d043
Henley Hamilton <henley.hamilton@gmail.com>
2019-08-08 14:57:44 +1000

    (Rule 08) Converted tabs to 4-spaces in FixBookUI

--- src/FixBookUI.java
+++ src/FixBookUI.java
@@ -3,86 +3,86 @@ import java.util.Scanner;
 
 public class FixBookUI {
 
-	public static enum UIState { INITIALISED, READY, FIXING, COMPLETED };
+    public static enum UIState { INITIALISED, READY, FIXING, COMPLETED };
 
-	private FixBookControl CoNtRoL;
-	private Scanner input;
-	private UIState StAtE;
-
-	
-	public FixBookUI(FixBookControl control) {
-		this.CoNtRoL = control;
-		input = new Scanner(System.in);
-		StAtE = UIState.INITIALISED;
-		control.Set_Ui(this);
-	}
+    private FixBookControl CoNtRoL;
+    private Scanner input;
+    private UIState StAtE;
 
 
-	public void Set_State(UIState state) {
-		this.StAtE = state;
-	}
+    public FixBookUI(FixBookControl control) {
+        this.CoNtRoL = control;
+        input = new Scanner(System.in);
+        StAtE = UIState.INITIALISED;
+        control.Set_Ui(this);
+    }
 
-	
-	public void RuN() {
-		output("Fix Book Use Case UI\n");
-		
-		while (true) {
-			
-			switch (StAtE) {
-			
-			case READY:
-				String Book_STR = input("Scan Book (<enter> completes): ");
-				if (Book_STR.length() == 0) {
-					CoNtRoL.SCannING_COMplete();
-				}
-				else {
-					try {
-						int Book_ID = Integer.valueOf(Book_STR).intValue();
-						CoNtRoL.Book_scanned(Book_ID);
-					}
-					catch (NumberFormatException e) {
-						output("Invalid bookId");
-					}
-				}
-				break;	
-				
-			case FIXING:
-				String AnS = input("Fix Book? (Y/N) : ");
-				boolean FiX = false;
-				if (AnS.toUpperCase().equals("Y")) {
-					FiX = true;
-				}
-				CoNtRoL.FIX_Book(FiX);
-				break;
-								
-			case COMPLETED:
-				output("Fixing process complete");
-				return;
-			
-			default:
-				output("Unhandled state");
-				throw new RuntimeException("FixBookUI : unhandled state :" + StAtE);			
-			
-			}		
-		}
-		
-	}
 
-	
-	private String input(String prompt) {
-		System.out.print(prompt);
-		return input.nextLine();
-	}	
-		
-		
-	private void output(Object object) {
-		System.out.println(object);
-	}
-	
+    public void Set_State(UIState state) {
+        this.StAtE = state;
+    }
 
-	public void display(Object object) {
-		output(object);
-	}
-	
-	
+
+    public void RuN() {
+        output("Fix Book Use Case UI\n");
+
+        while (true) {
+
+            switch (StAtE) {
+
+            case READY:
+                String Book_STR = input("Scan Book (<enter> completes): ");
+                if (Book_STR.length() == 0) {
+                    CoNtRoL.SCannING_COMplete();
+                }
+                else {
+                    try {
+                        int Book_ID = Integer.valueOf(Book_STR).intValue();
+                        CoNtRoL.Book_scanned(Book_ID);
+                    }
+                    catch (NumberFormatException e) {
+                        output("Invalid bookId");
+                    }
+                }
+                break;
+
+            case FIXING:
+                String AnS = input("Fix Book? (Y/N) : ");
+                boolean FiX = false;
+                if (AnS.toUpperCase().equals("Y")) {
+                    FiX = true;
+                }
+                CoNtRoL.FIX_Book(FiX);
+                break;
+
+            case COMPLETED:
+                output("Fixing process complete");
+                return;
+
+            default:
+                output("Unhandled state");
+                throw new RuntimeException("FixBookUI : unhandled state :" + StAtE);
+
+            }
+        }
+
+    }
+
+
+    private String input(String prompt) {
+        System.out.print(prompt);
+        return input.nextLine();
+    }
+
+
+    private void output(Object object) {
+        System.out.println(object);
+    }
+
+
+    public void display(Object object) {
+        output(object);
+    }
+
+
 }
