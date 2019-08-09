ec6e089ffa66f234383ee971ff06e4dc4e4ef92c
cmougios <cmougios@tpg.com.au>
2019-08-10 03:51:27 +1000

    refactored String name from MEM_STR -> memberCard

--- BorrowBookUI.java
+++ BorrowBookUI.java
@@ -47,13 +47,13 @@ public class BorrowBookUI {
 
 				
 			case READY:
-				String MEM_STR = input("Swipe member card (press <enter> to cancel): ");
-				if (MEM_STR.length() == 0) {
+				String memberCard = input("Swipe member card (press <enter> to cancel): ");
+				if (memberCard.length() == 0) {
 					control.cancel();
 					break;
 				}
 				try {
-					int Member_ID = Integer.valueOf(MEM_STR).intValue();
+					int Member_ID = Integer.valueOf(memberCard).intValue();
 					control.swiped(Member_ID);
 				}
 				catch (NumberFormatException e) {
