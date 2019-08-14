286180312a6b20daf6b698899edb82700acafe73
thejocus <b4rben@gmail.com>
2019-08-14 15:14:05 +1000

    Renamed PayFineControl member enum CONTROL_STATE -> ControlState

--- PayFineControl.java
+++ PayFineControl.java
@@ -1,8 +1,8 @@
 public class PayFineControl {
 	
 	private PayFineUI controlInstance;
-	private enum CONTROL_STATE { INITIALISED, READY, PAYING, COMPLETED, CANCELLED };
-	private CONTROL_STATE StAtE;
+	private enum ControlState { INITIALISED, READY, PAYING, COMPLETED, CANCELLED };
+	private ControlState StAtE;
 	
 	private library LiBrArY;
 	private member MeMbEr;
@@ -10,22 +10,22 @@ public class PayFineControl {
 
 	public PayFineControl() {
 		this.LiBrArY = LiBrArY.INSTANCE();
-		StAtE = CONTROL_STATE.INITIALISED;
+		StAtE = ControlState.INITIALISED;
 	}
 	
 	
 	public void Set_UI(PayFineUI ui) {
-		if (!StAtE.equals(CONTROL_STATE.INITIALISED)) {
+		if (!StAtE.equals(ControlState.INITIALISED)) {
 			throw new RuntimeException("PayFineControl: cannot call setUI except in INITIALISED state");
 		}	
 		this.controlInstance = ui;
 		ui.Set_State(PayFineUI.UI_STATE.READY);
-		StAtE = CONTROL_STATE.READY;
+		StAtE = ControlState.READY;
 	}
 
 
 	public void Card_Swiped(int memberId) {
-		if (!StAtE.equals(CONTROL_STATE.READY)) {
+		if (!StAtE.equals(ControlState.READY)) {
 			throw new RuntimeException("PayFineControl: cannot call cardSwiped except in READY state");
 		}	
 		MeMbEr = LiBrArY.MEMBER(memberId);
@@ -36,18 +36,18 @@ public class PayFineControl {
 		}
 		controlInstance.DiSplAY(MeMbEr.toString());
 		controlInstance.Set_State(PayFineUI.UI_STATE.PAYING);
-		StAtE = CONTROL_STATE.PAYING;
+		StAtE = ControlState.PAYING;
 	}
 	
 	
 	public void CaNcEl() {
 		controlInstance.Set_State(PayFineUI.UI_STATE.CANCELLED);
-		StAtE = CONTROL_STATE.CANCELLED;
+		StAtE = ControlState.CANCELLED;
 	}
 
 
 	public double PaY_FiNe(double AmOuNt) {
-		if (!StAtE.equals(CONTROL_STATE.PAYING)) {
+		if (!StAtE.equals(ControlState.PAYING)) {
 			throw new RuntimeException("PayFineControl: cannot call payFine except in PAYING state");
 		}	
 		double ChAnGe = MeMbEr.Pay_Fine(AmOuNt);
@@ -56,7 +56,7 @@ public class PayFineControl {
 		}
 		controlInstance.DiSplAY(MeMbEr.toString());
 		controlInstance.Set_State(PayFineUI.UI_STATE.COMPLETED);
-		StAtE = CONTROL_STATE.COMPLETED;
+		StAtE = ControlState.COMPLETED;
 		return ChAnGe;
 	}
 	
