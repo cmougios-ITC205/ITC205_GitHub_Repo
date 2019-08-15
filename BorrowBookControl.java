71607a143afe413c7c10fa3dbd9fcbb1e4344fc8
cmougios <cmougios@tpg.com.au>
2019-08-15 22:20:08 +1000

    Changed indentation to 4 spaces

--- BorrowBookControl.java
+++ BorrowBookControl.java
@@ -2,116 +2,116 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BorrowBookControl {
-	
-	private BorrowBookUI UI;
-	
-	private library library;
-	private member member;
-	private enum ControlState { INITIALISED, READY, RESTRICTED, SCANNING, IDENTIFIED, FINALISING, COMPLETED, CANCELLED };
-	private ControlState state;
-	
-	private List<book> pending;
-	private List<loan> completed;
-	private book book;
-	
-	
-	public BorrowBookControl() {
-		this.library = library.INSTANCE();
-		this.state = ControlState.INITIALISED;
-	}
-	
 
-	public void setUI(BorrowBookUI UI) {
-		if (!this.state.equals(ControlState.INITIALISED))
-			throw new RuntimeException("BorrowBookControl: cannot call setUI except in INITIALISED state");
-			
-		this.UI = UI;
-		UI.setState(BorrowBookUI.UiState.READY);
-		this.state = ControlState.READY;
-	}
+    private BorrowBookUI UI;
 
-		
-	public void swiped(int memberId) {
-		if (!this.state.equals(ControlState.READY))
-			throw new RuntimeException("BorrowBookControl: cannot call cardSwiped except in READY state");
-			
-		this.member = library.MEMBER(memberId);
-		if (this.member == null) {
-			this.UI.display("Invalid memberId");
-			return;
-		}
-		if (this.library.MEMBER_CAN_BORROW(this.member)) {
-			this.pending = new ArrayList<>();
-			this.UI.setState(BorrowBookUI.UiState.SCANNING);
-			this.state = ControlState.SCANNING; }
-		else 
-		{
-			this.UI.display("Member cannot borrow at this time");
-			this.UI.setState(BorrowBookUI.UiState.RESTRICTED); }}
-	
-	
-	public void scanned(int bookId) {
-		this.book = null;
-		if (!this.state.equals(ControlState.SCANNING)) {
-			throw new RuntimeException("BorrowBookControl: cannot call bookScanned except in SCANNING state");
-		}	
-		this.book = this.library.Book(bookId);
-		if (this.book == null) {
-			this.UI.display("Invalid bookId");
-			return;
-		}
-		if (!this.book.AVAILABLE()) {
-			this.UI.display("Book cannot be borrowed");
-			return;
-		}
-		this.pending.add(this.book);
-		for (book displayBook : this.pending) {
-			this.UI.display(displayBook.toString());
-		}
-		if (this.library.Loans_Remaining_For_Member(this.member) - this.pending.size() == 0) {
-			this.UI.display("Loan limit reached");
-			this.complete();
-		}
-	}
-	
-	
-	public void complete() {
-		if (this.pending.size() == 0) {
-			this.cancel();
-		}
-		else {
-			this.UI.display("\nFinal Borrowing List");
-			for (book displayBook : this.pending) {
-				this.UI.display(displayBook.toString());
-			}
-			this.completed = new ArrayList<loan>();
-			this.UI.setState(BorrowBookUI.UiState.FINALISING);
-			this.state = ControlState.FINALISING;
-		}
-	}
+    private library library;
+    private member member;
+    private enum ControlState { INITIALISED, READY, RESTRICTED, SCANNING, IDENTIFIED, FINALISING, COMPLETED, CANCELLED };
+    private ControlState state;
+
+    private List<book> pending;
+    private List<loan> completed;
+    private book book;
 
 
-	public void commitLoans() {
-		if (!this.state.equals(ControlState.FINALISING)) {
-			throw new RuntimeException("BorrowBookControl: cannot call commitLoans except in FINALISING state");
-		}	
-		for (book displayBook : this.pending) {
-			loan LOAN = this.library.ISSUE_LAON(displayBook, this.member);
-			this.completed.add(LOAN);
-		}
-		this.UI.display("Completed Loan Slip");
-		for (loan LOAN : this.completed) {
-			this.UI.display(LOAN.toString());
-		}
-		this.UI.setState(BorrowBookUI.UiState.COMPLETED);
-		this.state = ControlState.COMPLETED;
-	}
+    public BorrowBookControl() {
+        this.library = library.INSTANCE();
+        this.state = ControlState.INITIALISED;
+    }
 
-	
-	public void cancel() {
-		this.UI.setState(BorrowBookUI.UiState.CANCELLED);
-		this.state = ControlState.CANCELLED;
-	}
-	
-	
+
+    public void setUI(BorrowBookUI UI) {
+        if (!this.state.equals(ControlState.INITIALISED))
+            throw new RuntimeException("BorrowBookControl: cannot call setUI except in INITIALISED state");
+
+        this.UI = UI;
+        UI.setState(BorrowBookUI.UiState.READY);
+        this.state = ControlState.READY;
+    }
+
+
+    public void swiped(int memberId) {
+        if (!this.state.equals(ControlState.READY))
+            throw new RuntimeException("BorrowBookControl: cannot call cardSwiped except in READY state");
+
+        this.member = library.MEMBER(memberId);
+        if (this.member == null) {
+            this.UI.display("Invalid memberId");
+            return;
+        }
+        if (this.library.MEMBER_CAN_BORROW(this.member)) {
+            this.pending = new ArrayList<>();
+            this.UI.setState(BorrowBookUI.UiState.SCANNING);
+            this.state = ControlState.SCANNING; }
+        else
+        {
+            this.UI.display("Member cannot borrow at this time");
+            this.UI.setState(BorrowBookUI.UiState.RESTRICTED); }}
+
+
+    public void scanned(int bookId) {
+        this.book = null;
+        if (!this.state.equals(ControlState.SCANNING)) {
+            throw new RuntimeException("BorrowBookControl: cannot call bookScanned except in SCANNING state");
+        }
+        this.book = this.library.Book(bookId);
+        if (this.book == null) {
+            this.UI.display("Invalid bookId");
+            return;
+        }
+        if (!this.book.AVAILABLE()) {
+            this.UI.display("Book cannot be borrowed");
+            return;
+        }
+        this.pending.add(this.book);
+        for (book displayBook : this.pending) {
+            this.UI.display(displayBook.toString());
+        }
+        if (this.library.Loans_Remaining_For_Member(this.member) - this.pending.size() == 0) {
+            this.UI.display("Loan limit reached");
+            this.complete();
+        }
+    }
+
+
+    public void complete() {
+        if (this.pending.size() == 0) {
+            this.cancel();
+        }
+        else {
+            this.UI.display("\nFinal Borrowing List");
+            for (book displayBook : this.pending) {
+                this.UI.display(displayBook.toString());
+            }
+            this.completed = new ArrayList<loan>();
+            this.UI.setState(BorrowBookUI.UiState.FINALISING);
+            this.state = ControlState.FINALISING;
+        }
+    }
+
+
+    public void commitLoans() {
+        if (!this.state.equals(ControlState.FINALISING)) {
+            throw new RuntimeException("BorrowBookControl: cannot call commitLoans except in FINALISING state");
+        }
+        for (book displayBook : this.pending) {
+            loan LOAN = this.library.ISSUE_LAON(displayBook, this.member);
+            this.completed.add(LOAN);
+        }
+        this.UI.display("Completed Loan Slip");
+        for (loan LOAN : this.completed) {
+            this.UI.display(LOAN.toString());
+        }
+        this.UI.setState(BorrowBookUI.UiState.COMPLETED);
+        this.state = ControlState.COMPLETED;
+    }
+
+
+    public void cancel() {
+        this.UI.setState(BorrowBookUI.UiState.CANCELLED);
+        this.state = ControlState.CANCELLED;
+    }
+
+
 }
--- BorrowBookUI.java
+++ BorrowBookUI.java
@@ -2,117 +2,116 @@ import java.util.Scanner;
 
 
 public class BorrowBookUI {
-	
-	public static enum UiState { INITIALISED, READY, RESTRICTED, SCANNING, IDENTIFIED, FINALISING, COMPLETED, CANCELLED };
 
-	private BorrowBookControl control;
-	private Scanner input;
-	private UiState state;
+    public static enum UiState { INITIALISED, READY, RESTRICTED, SCANNING, IDENTIFIED, FINALISING, COMPLETED, CANCELLED };
 
-	
-	public BorrowBookUI(BorrowBookControl control) {
-		this.control = control;
-		this.input = new Scanner(System.in);
-		this.state = UiState.INITIALISED;
-		this.control.setUI(this);
-	}
-
-	
-	private String input(String prompt) {
-		System.out.print(prompt);
-		return this.input.nextLine();
-	}	
-		
-		
-	private void output(Object object) {
-		System.out.println(object);
-	}
-	
-			
-	public void setState(UiState state) {
-		this.state = state;
-	}
-
-	
-	public void run() {
-		this.output("Borrow Book Use Case UI\n");
-		
-		while (true) {
-			
-			switch (this.state) {
-			
-			case CANCELLED:
-				this.output("Borrowing Cancelled");
-				return;
-
-				
-			case READY:
-				String memberCard = input("Swipe member card (press <enter> to cancel): ");
-				if (memberCard.length() == 0) {
-					this.control.cancel();
-					break;
-				}
-				try {
-					int memberId = Integer.valueOf(memberCard).intValue();
-					this.control.swiped(memberId);
-				}
-				catch (NumberFormatException e) {
-					this.output("Invalid Member Id");
-				}
-				break;
-
-				
-			case RESTRICTED:
-				this.input("Press <any key> to cancel");
-				this.control.cancel();
-				break;
-			
-				
-			case SCANNING:
-				String scanBook = input("Scan Book (<enter> completes): ");
-				if (scanBook.length() == 0) {
-					this.control.complete();
-					break;
-				}
-				try {
-					int bookId = Integer.valueOf(scanBook).intValue();
-					this.control.scanned(bookId);
-					
-				} catch (NumberFormatException e) {
-					this.output("Invalid Book Id");
-				} 
-				break;
-					
-				
-			case FINALISING:
-				String answerQuestion = input("Commit loans? (Y/N): ");
-				if (answerQuestion.toUpperCase().equals("N")) {
-					this.control.cancel();
-					
-				} else {
-					this.control.commitLoans();
-					this.input("Press <any key> to complete ");
-				}
-				break;
-				
-				
-			case COMPLETED:
-				this.output("Borrowing Completed");
-				return;
-	
-				
-			default:
-				this.output("Unhandled state");
-				throw new RuntimeException("BorrowBookUI : unhandled state :" + this.state);
-			}
-		}		
-	}
+    private BorrowBookControl control;
+    private Scanner input;
+    private UiState state;
 
 
-	public void display(Object object) {
+    public BorrowBookUI(BorrowBookControl control) {
+        this.control = control;
+        this.input = new Scanner(System.in);
+        this.state = UiState.INITIALISED;
+        this.control.setUI(this);
+    }
 
-		this.output(object);
-	}
+
+    private String input(String prompt) {
+        System.out.print(prompt);
+        return this.input.nextLine();
+    }
+
+
+    private void output(Object object) {
+        System.out.println(object);
+    }
+
+
+    public void setState(UiState state) {
+        this.state = state;
+    }
+
+
+    public void run() {
+        this.output("Borrow Book Use Case UI\n");
+
+        while (true) {
+
+            switch (this.state) {
+
+            case CANCELLED:
+                this.output("Borrowing Cancelled");
+                return;
+
+
+            case READY:
+                String memberCard = input("Swipe member card (press <enter> to cancel): ");
+                if (memberCard.length() == 0) {
+                    this.control.cancel();
+                    break;
+                }
+                try {
+                    int memberId = Integer.valueOf(memberCard).intValue();
+                    this.control.swiped(memberId);
+                }
+                catch (NumberFormatException e) {
+                    this.output("Invalid Member Id");
+                }
+                break;
+
+
+            case RESTRICTED:
+                this.input("Press <any key> to cancel");
+                this.control.cancel();
+                break;
+
+
+            case SCANNING:
+                String scanBook = input("Scan Book (<enter> completes): ");
+                if (scanBook.length() == 0) {
+                    this.control.complete();
+                    break;
+                }
+                try {
+                    int bookId = Integer.valueOf(scanBook).intValue();
+                    this.control.scanned(bookId);
+
+                } catch (NumberFormatException e) {
+                    this.output("Invalid Book Id");
+                }
+                break;
+
+
+            case FINALISING:
+                String answerQuestion = input("Commit loans? (Y/N): ");
+                if (answerQuestion.toUpperCase().equals("N")) {
+                    this.control.cancel();
+
+                } else {
+                    this.control.commitLoans();
+                    this.input("Press <any key> to complete ");
+                }
+                break;
+
+
+            case COMPLETED:
+                this.output("Borrowing Completed");
+                return;
+
+
+            default:
+                this.output("Unhandled state");
+                throw new RuntimeException("BorrowBookUI : unhandled state :" + this.state);
+            }
+        }
+    }
+
+
+    public void display(Object object) {
+        this.output(object);
+    }
 
 
 }
